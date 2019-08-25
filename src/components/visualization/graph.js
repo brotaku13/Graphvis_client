a6362e9f2c9321e95cfdb00dbebc57adcaa8f052
Chau <ChauTNguyen96@gmail.com>
2019-08-24 18:07:12 -0700

    Set up random graph in /id

--- src/components/visualization/graph.js
+++ src/components/visualization/graph.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from 'react'
+import ForceGraph3d from 'react-force-graph-3d'
 import axios from 'axios'
+
+const makeRandomTree = (n = 300) => ({
+  nodes: [...Array(n).keys()].map(i => ({ id: i })),
+  links: [...Array(n).keys()]
+    .filter(id => id)
+    .map(id => ({ source: id, target: Math.round(Math.random() * (id - 1)) })),
+})
 
 const Graph = ({ match }) => {
   const [graphData, setGraphData] = useState({
@@ -22,7 +30,12 @@ const Graph = ({ match }) => {
     })()
   })
 
-  return <div>ID: {id}</div>
+  return (
+    <div>
+      <div>ID: {id}</div>
+      <ForceGraph3d graphData={makeRandomTree()} />
+    </div>
+  )
 }
 
 export default Graph
