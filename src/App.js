fafe07ce0239ea84a6b6783c1861677f7e3d0316
brotaku <brian.caulfield13@gmail.com>
2019-08-26 18:35:47 -0700

    finished converting Home component over to functional design with hooks.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 
-import HomePage from './components/homepage'
+import Home from './components/homepage'
 // ^ index.js hurts IDE path intellisense
 import Graph from './components/visualization/graph'
 import Explore from './components/explore/explore'
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={Home} />
         <Route path="/id/:id" component={Graph} />
         <Route path="/explore" component={Explore} />
       </BrowserRouter>
--- src/components/homepage/index.js
+++ src/components/homepage/index.js
@@ -1,162 +1,166 @@
-import React, { Component } from 'react'
-import Paper from '@material-ui/core/Paper'
-import TextField from '@material-ui/core/TextField'
-import Button from '@material-ui/core/Button'
-import Slide from '@material-ui/core/Slide'
+import React, { useState, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Slide from "@material-ui/core/Slide";
+import Typography from "@material-ui/core/Typography";
 
-import bg from '../../assets/images/graph_background.jpg'
-import UploadContainer from './UploadContainer';
+import bg from "../../assets/images/graph_background.jpg";
+import UploadContainer from "./UploadContainer";
 
 const styles = {
   root: {
     backgroundImage: `url(${bg})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: '50%',
-    position: 'absolute',
-    width: '100%',
-    height: '100%',
-    overlay: 'hidden',
-    overflow: 'hidden',
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "50%",
+    position: "absolute",
+    width: "100%",
+    height: "100%",
+    overlay: "hidden",
+    overflow: "hidden"
   },
   content: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "flex-start",
+    flexDirection: "column",
+    alignItems: "center"
   },
   container: {
-    display: 'flex',
+    display: "flex"
   },
   loginPaper: {
     zIndex: 10,
-    width: '50vw',
-    display: 'flex',
-    padding: '1em',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
+    width: "50vw",
+    display: "flex",
+    padding: "1em",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center"
   },
   loginTitle: {
     zIndex: 10,
-    marginTop: '2em',
-    marginBottom: '1.5em',
-    fontSize: '2em',
+    marginTop: "1.5em",
+    marginBottom: "0.5em",
+    fontSize: "2em"
   },
   graphIDTextfield: {
-    width: '80%',
-    marginBottom: '2em',
+    width: "80%",
+    marginBottom: "2em"
   },
-  newGraphButtonGroup: {},
   error: {
-    marginTop: '5%',
-    color: '#ff1744',
-    fontSize: '.8em',
+    marginTop: "5%",
+    color: "#ff1744",
+    fontSize: ".8em"
   },
-}
-
-class HomePage extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      graph_id: '',
-      graphIDDisplay: false,
-      newGraphDisplay: true,
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.onChange = this.onChange.bind(this)
-    this.handleNewGraphNav = this.handleNewGraphNav.bind(this)
-    this.handleCancel = this.handleCancel.bind(this)
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: '100%'
+  },
+  buttonGroup: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  navButton:{
+    marginLeft: '1em',
+    marginRight: '1em'
   }
+};
 
-  handleSubmit(event) {
-    event.preventDefault()
-    alert(this.state.graph_id)
-  }
+const Home = () => {
+  const [graphId, setGraphId] = useState("");
+  const [showUi, setShowUi] = useState({
+    about: false,
+    home: true,
+    new: false
+  });
 
-  onChange(event) {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+  const handleNewGraph = () => {
+    setShowUi({
+      ...showUi,
+      home: false,
+      new: true
+    });
+  };
 
-  handleNewGraphNav() {
-    this.setState({
-      graphIDDisplay: false,
-      newGraphDisplay: true,
-    })
-  }
+  const handleCancel = () => {
+    setShowUi({
+      ...showUi,
+      home: true,
+      new: false
+    });
+  };
 
-  handleCancel() {
-    this.setState({
-      graphIDDisplay: true,
-      newGraphDisplay: false,
-    })
-  }
+  const handleSubmit = () => {};
 
-  render() {
-    return (
-      <div className="graph-root" style={styles.root}>
-        <div style={styles.content}>
-          <div className="title" style={styles.title}>
-            Graphvis
+  const onTextField = e => {
+    setGraphId(e.target.value);
+  };
+
+  return (
+    <div className="graph-root" style={styles.root}>
+      <div style={styles.content}>
+        <div className="title">
+          <Typography variant="h1">Graphvis</Typography>
         </div>
-          <div style={styles.container}>
-            <div>
-              <Slide
-                direction="right"
-                in={this.state.graphIDDisplay}
-                mountOnEnter
-                unmountOnExit
-              >
-                <Paper style={styles.loginPaper} elevation={4}>
-                  <div className="montserrat" style={styles.loginTitle}>
-                    Enter a Graph ID
+        <div style={styles.container}>
+          <div>
+            <Slide
+              direction="right"
+              in={showUi.home}
+              mountOnEnter
+              unmountOnExit
+            >
+              <Paper style={styles.loginPaper} elevation={4}>
+                <div style={styles.loginTitle}>
+                  <Typography variant="h4">Enter a Graph ID</Typography>
                 </div>
-                  <div style={styles.graphIDTextfield}>
-                    <form onSubmit={this.handleSubmit}>
-                      <TextField
-                        id="graph_id_textfield"
-                        name="graph_id"
-                        label="Graph ID"
-                        palceholder="Enter here"
-                        fullWidth
-                        margin="normal"
-                        variant="outlined"
-                        onChange={this.onChange}
-                        InputLabelProps={{
-                          shrink: true,
-                        }}
-                      />
-                    </form>
+                <div style={styles.graphIDTextfield}>
+                  <form onSubmit={handleSubmit}>
+                    <TextField
+                      id="graph_id_textfield"
+                      name="graph_id"
+                      label="Graph ID"
+                      palceholder="Enter here"
+                      fullWidth
+                      margin="normal"
+                      variant="outlined"
+                      onChange={onTextField}
+                      InputLabelProps={{
+                        shrink: true
+                      }}
+                    />
+                  </form>
+                </div>
+                <div style={styles.buttonContainer}>
+                  <Typography>Don't have a graph ID?</Typography>
+                  <div style={styles.buttonGroup}>
+                    <Button color="primary" style={styles.navButton} onClick={handleNewGraph}>
+                      Create
+                    </Button>
+                    <Button color="primary" style={styles.navButton} onClick={handleNewGraph}>
+                      Explore
+                    </Button>
                   </div>
-                  <div className="montserrat" style={styles.newGraphButtonGroup}>
-                    Don't have a graph ID?
-                  <Button color="primary" onClick={this.handleNewGraphNav}>
-                      Create a new graph
-                  </Button>
-                  </div>
-                </Paper>
-              </Slide>
-            </div>
-            <div>
-              <Slide
-                direction="left"
-                in={this.state.newGraphDisplay}
-                mountOnEnter
-                unmountOnExit
-              >
-                <Paper style={styles.loginPaper} elevation={4}>
-                  {/* <GraphUpload handleCancel={this.handleCancel} /> */}
-                  <UploadContainer
-                    cancel={this.handleCancel}
-                  />
-                </Paper>
-              </Slide>
-            </div>
+                </div>
+              </Paper>
+            </Slide>
+          </div>
+          <div>
+            <Slide direction="left" in={showUi.new} mountOnEnter unmountOnExit>
+              <Paper style={styles.loginPaper} elevation={4}>
+                <UploadContainer cancel={handleCancel} />
+              </Paper>
+            </Slide>
           </div>
         </div>
-      </div >
-    )
-  }
-}
+      </div>
+    </div>
+  );
+};
 
-export default HomePage
+export default Home;
