ba31ce174b177b89c679f79ee8fda307934119d2
brotaku <brian.caulfield13@gmail.com>
2019-08-28 22:20:33 -0700

    Linked the cameras

--- src/components/visualization/GraphContainer.js
+++ src/components/visualization/GraphContainer.js
@@ -1,92 +1,171 @@
-import React, { useState, useEffect } from "react";
-import cytoscape from 'cytoscape'
+import React, { useState, useEffect } from 'react';
+import cytoscape from 'cytoscape';
 
-import Graph from "./graph";
+import Graph from './graph';
 
-import { getGraph } from "../../utils/GraphvisAPI";
+import { getGraph } from '../../utils/GraphvisAPI';
 
 const styles = {
   root: {
-    display: "flex",
-    flexDirection: "row"
-  }
+    display: 'flex',
+    flexDirection: 'row',
+  },
 };
 
-const GraphData = () =>{
+const GraphData = () => {
   return {
     cy: undefined,
-    nodes: undefined, 
-    edges: undefined
-  }
-}
+    nodes: undefined,
+    edges: undefined,
+  };
+};
 
 const Node = (id, abbrevName, fullName, x, y, z, orbits) => {
   return {
-    id: id, 
-    abbrevName: abbrevName, 
-    fullName: fullName, 
-    fx: x, 
-    fy: y, 
+    id: id,
+    abbrevName: abbrevName,
+    fullName: fullName,
+    fx: x,
+    fy: y,
     fz: z,
     orbits: orbits,
-    selected: false, 
-    color: {}
-  }
-}
+    selected: false,
+    color: {},
+  };
+};
 
 const Edge = (id, from, to, weight) => {
   return {
     id: id,
-    source: from, 
-    target: to, 
+    source: from,
+    target: to,
     weight: weight,
     color: {},
-    selected: false
-  }
-}
+    selected: false,
+  };
+};
 
-const buildGraphComponents = (nodes, graphData) =>{
-    let cyElements = []
-    let forceGraphNodes = []
-    let forceGraphEdges = []
+const buildGraphComponents = (nodes, graphData) => {
+  let cyElements = [];
+  let forceGraphNodes = [];
+  let forceGraphEdges = [];
 
-    let edgeTracker = {} // tracks from -> to
+  let edgeTracker = {}; // tracks from -> to
 
-    for(let i = 0; i < nodes.length; ++i){
-      let n = nodes[i];
-      let newNode = Node(n.id, n.abbrevName, n.fullName, ...Object.values(n.coordinates), n.orbits);
-      forceGraphNodes.push(newNode);
-      cyElements.push({data: newNode})
-      let edges = n.edges
-      for(let j = 0; j < edges.length; ++j){
-        let e = edges[j];
-        
-        if(e.to in edgeTracker && edgeTracker[e.to].contains(n.id)){
-          //already made this edge
-          console.log(e)
+  for (let i = 0; i < nodes.length; ++i) {
+    let n = nodes[i];
+    let newNode = Node(
+      n.id,
+      n.abbrevName,
+      n.fullName,
+      ...Object.values(n.coordinates),
+      n.orbits,
+    );
+    forceGraphNodes.push(newNode);
+    cyElements.push({ data: newNode });
+    let edges = n.edges;
+    for (let j = 0; j < edges.length; ++j) {
+      let e = edges[j];
+
+      if (e.to in edgeTracker && edgeTracker[e.to].contains(n.id)) {
+        //already made this edge
+        console.log(e);
+      } else {
+        let newEdge = Edge(e._id, n.id, e.to, e.weight);
+        forceGraphEdges.push(newEdge);
+        cyElements.push({ data: newEdge });
+
+        if (n.id in edgeTracker) {
+          edgeTracker[n.id].add(e.to);
         } else {
-          let newEdge = Edge(e._id, n.id, e.to, e.weight);
-          forceGraphEdges.push(newEdge);
-          cyElements.push({data: newEdge});
-
-          if(n.id in edgeTracker){
-            edgeTracker[n.id].add(e.to)
-          } else {
-            edgeTracker[n.id] = new Set([e.to])
-          }
+          edgeTracker[n.id] = new Set([e.to]);
         }
       }
     }
-    graphData.cy = cytoscape({elements: cyElements});
-    graphData.nodes = forceGraphNodes;
-    graphData.edges = forceGraphEdges
-}
+  }
+  graphData.cy = cytoscape({ elements: cyElements });
+  graphData.nodes = forceGraphNodes;
+  graphData.edges = forceGraphEdges;
+};
 
+const moveCamera = (pos, graph, lookat) => {
+  graph.cameraPosition(
+    {
+      x: pos.x,
+      y: pos.y,
+      x: pos.z,
+    },
+    lookat,
+  );
+};
+
+const linkCameras = (ocd, con) => {
+  let ocdCam = ocd.camera();
+  let conCam = ocd.camera();
+  console.log(ocdCam);
+  debugger;
+  ocdCam.matrix = conCam.matrix;
+  ocdCam.rotation = conCam.rotation;
+  ocdCam.quaternions = conCam.quaternions;
+  ocdCam.up = conCam.up;
+
+  ocd.addEventListener('change', e => {
+    moveCamera(e.target.object.position, ocd, con.cameraPosition().lookat);
+  });
+
+  con.addEventListener('change', e => {
+    moveCamera(e.target.object.position, con, ocd.cameraPosition().lookat);
+  });
+};
 
 const GraphContainer = props => {
   const [isLoading, setIsLoading] = useState(true);
-  const [ocdGraph, setOcdGraph] = useState(GraphData())
-  const [conGraph, setConGraph] = useState(GraphData())
+  const [ocdGraph, setOcdGraph] = useState(GraphData());
+  const [conGraph, setConGraph] = useState(GraphData());
+
+  var ocdGraphRef = null;
+  var conGraphRef = null;
+
+  const setCamera = (ref, name) => {
+    if (name === 'ocd') {
+      ocdGraphRef = ref;
+    } else {
+      conGraphRef = ref;
+    }
+
+    if (conGraphRef && ocdGraphRef) {
+      ocdGraphRef.camera().up = conGraphRef.camera().up;
+      ocdGraphRef.camera().matrix = conGraphRef.camera().matrix;
+
+      // linkCameras(conGraphRef, ocdGraphRef);
+      conGraphRef.controls().addEventListener('change', event => {
+        moveCamera(
+          event.target.object.position,
+          ocdGraphRef,
+          conGraphRef.cameraPosition().lookat,
+        );
+      });
+
+      ocdGraphRef.controls().addEventListener('change', event => {
+        moveCamera(
+          event.target.object.position,
+          conGraphRef,
+          ocdGraphRef.cameraPosition().lookat,
+        );
+      });
+    }
+  };
+
+  function moveCamera(position, graph, lookat) {
+    graph.cameraPosition(
+      {
+        x: position.x,
+        y: position.y,
+        z: position.z,
+      },
+      lookat,
+    );
+  }
 
   useEffect(() => {
     getGraph(props.graphId).then(res => {
@@ -100,14 +179,27 @@ const GraphContainer = props => {
     });
   }, []);
 
-
-  if (isLoading && ocdGraph.edges === undefined && ocdGraph.nodes === undefined) {
+  if (
+    isLoading &&
+    ocdGraph.edges === undefined &&
+    ocdGraph.nodes === undefined
+  ) {
     return <div>Loading...</div>;
   } else {
     return (
-      <div style={styles.root} >
-        <Graph name="ocd" nodes={ocdGraph.nodes} edges={ocdGraph.edges}/>
-        <Graph name="con" nodes={conGraph.nodes} edges={conGraph.edges}/>
+      <div style={styles.root}>
+        <Graph
+          name="ocd"
+          nodes={ocdGraph.nodes}
+          edges={ocdGraph.edges}
+          setCamera={setCamera}
+        />
+        <Graph
+          name="con"
+          nodes={conGraph.nodes}
+          edges={conGraph.edges}
+          setCamera={setCamera}
+        />
       </div>
     );
   }
--- src/components/visualization/graph.js
+++ src/components/visualization/graph.js
@@ -1,37 +1,44 @@
-import React from 'react'
-import ForceGraph3d from 'react-force-graph-3d'
+import React, { useState, useEffect } from 'react';
+import ForceGraph3d from 'react-force-graph-3d';
 
 import useWindowDimensions from './useWindowDimensions';
 
-//use this for displaying data only. 
+//use this for displaying data only.
 //any graph data manipulation should be done in the container class
 const nodeSize = 0.15;
 const nodeResolution = 30;
 
-const Graph = (props) => {
+const Graph = props => {
   console.log(props);
-  const {height, width} = useWindowDimensions();
+  var ref = null;
+  const { height, width } = useWindowDimensions();
 
   const getNodeLabel = node => {
     return node.id;
-  }
+  };
   const getNodeColor = node => {
-    return 'blue'
-  }
-  const onNodeClick = node => {
-
-  }
+    return 'blue';
+  };
+  const onNodeClick = node => {};
   const getEdgeColor = edge => {
-    return 'black'
-  }
+    return 'black';
+  };
   const getEdgeWidth = edge => {
-    return 0
-  }
+    return 0;
+  };
+
+  useEffect(() => {
+    //component did mount
+    props.setCamera(ref, props.name);
+  }, []);
 
   return (
-    <ForceGraph3d 
-      graphData={{nodes: props.nodes, links: props.edges}}
-      backgroundColor='White'
+    <ForceGraph3d
+      ref={el => {
+        ref = el;
+      }}
+      graphData={{ nodes: props.nodes, links: props.edges }}
+      backgroundColor="White"
       nodeVal={nodeSize}
       nodeLabel={node => getNodeLabel(node)}
       nodeColor={node => getNodeColor(node)}
@@ -40,9 +47,9 @@ const Graph = (props) => {
       onNodeClick={node => onNodeClick(node)}
       linkColor={edge => getEdgeColor(edge)}
       linkWidth={edge => getEdgeWidth(edge)}
-      width={width/2}
-      />
-  )
-}
+      width={width / 2}
+    />
+  );
+};
 
-export default Graph
+export default Graph;
